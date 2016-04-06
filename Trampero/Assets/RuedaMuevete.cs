3f4283af3cc04bf112c9c6a322c1c783b5f8787f
Kindraman <manujamming@gmail.com>
2016-04-05 22:38:53 -0300

    Se agrega proyecto TRAMPERO

---
+++ Trampero/Assets/RuedaMuevete.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuedaMuevete : MonoBehaviour {
+
+	public Rigidbody ruedita;
+	// Use this for initialization
+	void Start () {
+		ruedita.velocity = new Vector3 (10, 0, 0);// * Time.time;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	
+	}
+}
